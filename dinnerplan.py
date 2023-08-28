d4c81b5b283de6bebd61b11a1d649e6a9a0f0976
Asbjørn Ørnemark Lægdsmand <acerbjorn@gmail.com>
2023-08-28 22:27:11 +0200

    fixed the logic error resulting in wrong length output

--- dinnerplan.py
+++ dinnerplan.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
 
-weeks = 20
+weeks = 40
 
 cooks = np.array(pd.read_csv("cooks.csv").columns)
 assigned_cooking_spots = np.random.permutation(cooks)
-while (np.size(assigned_cooking_spots) <= weeks*4) and (np.size(assigned_cooking_spots) % 2 != 0):
+while (np.size(assigned_cooking_spots) <= (weeks*4)) or (np.size(assigned_cooking_spots) % 2 != 0):
     random_order_cooks = np.random.permutation(cooks)
-    
     assigned_cooking_spots = np.concatenate((assigned_cooking_spots, random_order_cooks))
+
 
 assigned_cooking_spots = np.reshape(assigned_cooking_spots, [-1, 2])
 
